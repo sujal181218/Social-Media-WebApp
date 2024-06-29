d33816de031233857bcbbf478e036febdac503c7
sujal181218 <122476399+sujal181218@users.noreply.github.com>
2024-06-29 19:21:39 +0530

    Add files via upload

---
+++ src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData= [
+    {
+      name: "Minions",
+      shares: 97,
+    },
+    {
+      name: "Avengers",
+      shares: 80.5,
+    },
+    {
+      name: "Drake",
+      shares: 75.5,
+    },
+    {
+      name: "BillGates",
+      shares: 72,
+    },
+    {
+      name: "Elon Musk",
+      shares: 71.9,
+    },
+    {
+      name: "Donald Duck",
+      shares: 20,
+    },
+  ];
+  
